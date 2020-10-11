c1aa65b8eb34cccf440de2a1b6d37c149941750f
Lucas Almeida <lucasdealmeida.ss@gmail.com>
2020-10-11 12:33:57 -0300

    Updates

---
+++ precache-manifest.2a74c6e1bf079e05f55c9a9808262a8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f81307de79e5c76afaafd8098affe75",
+    "url": "/chatchart/index.html"
+  },
+  {
+    "revision": "656a547441281bbdc8d4",
+    "url": "/chatchart/static/js/2.88eb191e.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/chatchart/static/js/2.88eb191e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63f2ee7d29b607e0133",
+    "url": "/chatchart/static/js/main.81005bd2.chunk.js"
+  },
+  {
+    "revision": "d6582e1bccdc4fac65de",
+    "url": "/chatchart/static/js/runtime-main.b1611069.js"
+  }
+]);
